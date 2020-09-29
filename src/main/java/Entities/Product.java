006159ad60c42611a7258a1ce627a291a6687fdd
Rasmus Jansson <rasmusja@kth.se>
2020-09-29 16:14:30 +0200

    Product merge

--- src/main/java/Entities/Product.java
+++ src/main/java/Entities/Product.java
@@ -1,4 +1,57 @@
 package Entities;
 
 public class Product {
+    private int id;
+    private String name;
+    private int amount;
+    private int cost;
+    private String description;
+
+    public Product(int id, String name, int amount, int cost, String description) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.cost = cost;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
